a737613966fc9c07c796e30fb6a3fd453fde48b6
lzp1007 <lzp19990909@163.com>
2022-06-23 22:49:59 +0800

    add MainTest4.java

--- demo-module-1/src/com/duyi/MainTest3.java
+++ demo-module-1/src/com/duyi/MainTest3.java
@@ -2,6 +2,6 @@ package com.duyi;
 
 public class MainTest3 {
 	public static void main(String[] args) {
-		System.out.println("test 3");
+		System.out.println("hello");
 	}
 }
---
+++ demo-module-1/src/com/duyi/MainTest4.java
@@ -0,0 +1,9 @@
+package com.duyi;
+
+import java.sql.SQLOutput;
+
+public class MainTest4 {
+	public static void main(String[] args) {
+		System.out.println("I am test 4!");
+	}
+}
