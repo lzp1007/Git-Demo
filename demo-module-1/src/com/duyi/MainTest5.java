cafb7b113f6b27428d25ba55afd1fe96f22239f9
lzp1007 <lzp19990909@163.com>
2022-06-23 23:08:18 +0800

    add MainTest5.java

--- demo-module-1/src/com/duyi/MainTest5.java
+++ demo-module-1/src/com/duyi/MainTest5.java
@@ -3,5 +3,7 @@ package com.duyi;
 public class MainTest5 {
 	public static void main(String[] args) {
 		System.out.println("I am test 5!");
+		System.out.println("I am test 5!");
+		System.out.println("I am test 5!");
 	}
 }
