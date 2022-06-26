2409eb02fa5c07a6e3b72015363b036d88e3fb5c
lzp1007 <lzp19990909@163.com>
2022-06-26 10:10:46 +0800

    test annotaton and reflection

--- demo-module-1/src/com/duyi/MainTest5.java
+++ demo-module-1/src/com/duyi/MainTest5.java
@@ -1,9 +1,28 @@
 package com.duyi;
 
+import java.lang.annotation.*;
+
+
+@MyAnnotation
 public class MainTest5 {
+	@Override
+	@MyAnnotation(age = 22)
+	public String toString() {
+		
+		return super.toString();
+	}
 	public static void main(String[] args) {
 		System.out.println("I am test 5!");
 		System.out.println("I am test 5!");
 		System.out.println("I am test 5!");
+		
 	}
+}
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {ElementType.TYPE, ElementType.METHOD})
+@interface MyAnnotation {
+	int age() default 18;
+	String name() default "lzp";
 }
